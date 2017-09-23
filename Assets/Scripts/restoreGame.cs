f6b51e3b51cfd3e261386d9df520e7e99ff7ccc2
Guadalupe Sánchez <lupilla77@gmail.com>
2017-09-23 19:55:13 +0200

    mejora en script para restaurar el juego

--- Assets/Scripts/restoreGame.cs
+++ Assets/Scripts/restoreGame.cs
@@ -10,7 +10,12 @@ public class restoreGame : MonoBehaviour {
 		if (PlayerPrefs.GetInt ("Level") > 1) {
 			LevelManager.LevelCount = PlayerPrefs.GetInt ("Level");
 			LevelManager.force = PlayerPrefs.GetFloat ("Force");
-			SceneManager.LoadScene ("level0" + LevelManager.LevelCount);
+			if (LevelManager.LevelCount <= 9) {
+				SceneManager.LoadScene ("level0" + LevelManager.LevelCount);
+			} 
+			else {
+				SceneManager.LoadScene ("level" + LevelManager.LevelCount);
+			}
 		} else {
 			SceneManager.LoadScene("level01");
 		}
